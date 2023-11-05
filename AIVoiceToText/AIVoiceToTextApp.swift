581b503ccaa78df96dfbb4857e43ef5442a1f0bc
John goodstadt <johngoodstadt@icloud.com>
2023-11-05 10:37:08 +0000

    Initial Commit

---
+++ AIVoiceToText/AIVoiceToTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIVoiceToTextApp.swift
+//  AIVoiceToText
+//
+//  Created by John goodstadt on 05/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct AIVoiceToTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
